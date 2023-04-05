b7721c2c54c12ee477ec6b157d543c3312e4b669
Eric K <54124297+Nyveon@users.noreply.github.com>
2023-04-05 06:41:37 -0400

    github and email

--- docs/js/index.js
+++ docs/js/index.js
@@ -8,6 +8,7 @@ if ((deviceWidth = window.matchMedia("(max-width: 1024px)").matches)) {
 	setHeight();
 }
 
+// Light blob
 const blob = document.getElementById("blob");
 window.onpointermove = (event) => {
 	const { clientX, clientY } = event;
@@ -19,4 +20,39 @@ window.onpointermove = (event) => {
 		},
 		{ duration: 2000, fill: "forwards", easing: "ease-out" }
 	);
+};
+
+// Nameplate effect
+const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+
+let interval = null;
+
+document.getElementById("nametag").onmouseover = (event) => {
+	let iteration = 0;
+
+	clearInterval(interval);
+
+	let targetText =
+		event.target.innerText === event.target.dataset.first
+			? event.target.dataset.second
+			: event.target.dataset.first;
+
+	interval = setInterval(() => {
+		event.target.innerText = event.target.innerText
+			.split("")
+			.map((letter, index) => {
+				if (index < iteration) {
+					return targetText[index];
+				}
+
+				return letters[Math.floor(Math.random() * 26 * 2)];
+			})
+			.join("");
+
+		if (iteration >= targetText.length) {
+			clearInterval(interval);
+		}
+
+		iteration += 1 / 3;
+	}, 30);
 };
