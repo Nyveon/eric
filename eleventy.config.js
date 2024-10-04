ad3cb09168041b3bec2f00bc4e821b4b6c170305
Eric K <54124297+Nyveon@users.noreply.github.com>
2024-10-03 23:54:29 -0300

    chore: config for cfp

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,17 +1,15 @@
 import { IdAttributePlugin } from "@11ty/eleventy";
 
-
-
 export default function (eleventyConfig) {
-    eleventyConfig.addPlugin(IdAttributePlugin);
+	eleventyConfig.addPlugin(IdAttributePlugin);
 
 	eleventyConfig.addPassthroughCopy("./src/css");
 	eleventyConfig.addPassthroughCopy("./src/js");
 	eleventyConfig.addPassthroughCopy("./src/img");
 	eleventyConfig.addPassthroughCopy("./src/fonts");
 
-    eleventyConfig.setInputDirectory("src");
-    eleventyConfig.setDataDirectory("data");
-    eleventyConfig.setIncludesDirectory("includes");
-    eleventyConfig.setOutputDirectory("docs");
-};
+	eleventyConfig.setInputDirectory("src");
+	eleventyConfig.setDataDirectory("data");
+	eleventyConfig.setIncludesDirectory("includes");
+	eleventyConfig.setOutputDirectory("_site");
+}
