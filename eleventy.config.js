438b6fd5295dfe707eec69c07727c7f6c93f6f73
Eric K <54124297+Nyveon@users.noreply.github.com>
2024-10-23 08:12:29 -0300

    feat: minify and inline javascript

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,14 +1,24 @@
 import { IdAttributePlugin } from "@11ty/eleventy";
+import { minify } from "terser";
 
 export default function (eleventyConfig) {
 	eleventyConfig.addPlugin(IdAttributePlugin);
 
 	eleventyConfig.addPassthroughCopy("./src/css");
-	eleventyConfig.addPassthroughCopy("./src/js");
 	eleventyConfig.addPassthroughCopy("./src/img");
 	eleventyConfig.addPassthroughCopy("./src/fonts");
 	eleventyConfig.addPassthroughCopy({
 		"./src/config/robots.txt": "/robots.txt",
+	});
+
+	eleventyConfig.addFilter("jsmin", async function (code) {
+		try {
+			const minified = await minify(code);
+			return minified.code;
+		} catch (err) {
+			console.error("Terser error: ", err);
+			return code;
+		}
 	});
 
 	eleventyConfig.setInputDirectory("src");
--- src/js/nameplate.js
+++ src/js/nameplate.js
@@ -1,28 +1,3 @@
-// 100vh fix for mobile devices
-const setHeight = () => {
-	document.body.style.minHeight = window.innerHeight + "px";
-};
-
-if ((deviceWidth = window.matchMedia("(max-width: 1024px)").matches)) {
-	window.addEventListener("resize", setHeight);
-	setHeight();
-}
-
-// Light blob
-const blob = document.getElementById("blob");
-window.onpointermove = (event) => {
-	const { clientX, clientY } = event;
-
-	blob.animate(
-		{
-			left: `${clientX}px`,
-			top: `${clientY}px`,
-		},
-		{ duration: 2000, fill: "forwards", easing: "ease-out" }
-	);
-};
-
-// Nametag effect
 const nameplate = document.getElementById("nameplate");
 const letters = "abcdefghijklmnopqrstuvwxyz";
 
@@ -43,7 +18,7 @@ nameplate.onmouseenter = (event) => {
 	interval = setInterval(() => {
 		targetElement.innerText = targetElement.innerText
 			.split("")
-			.map((letter, index) => {
+			.map((_, index) => {
 				if (index < iteration) {
 					return targetText[index];
 				}
