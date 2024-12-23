65321c194e9d17514703955198e26cdcdbc4f2b5
Eric K <54124297+Nyveon@users.noreply.github.com>
2024-12-23 01:17:16 -0300

    feat: homepage

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,33 +1,38 @@
 // @ts-check
-// @ts-check
 import { defineConfig } from "astro/config";
+import icon from "astro-icon";
 // import { visualizer } from "rollup-plugin-visualizer";
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://eric.tc",
-	vite: {
-		css: {
-			preprocessorOptions: {
-				scss: {
-					api: "modern-compiler",
-				},
-			},
-		},
-		plugins: [
-			// visualizer({
-			// 	emitFile: false,
-			// 	filename: "stats.html",
-			// 	open: true,
-			// 	template: "sunburst",
-			// }),
-		],
+  site: "https://eric.tc",
+
+  vite: {
+      css: {
+          preprocessorOptions: {
+              scss: {
+                  api: "modern-compiler",
+              },
+          },
+      },
+      plugins: [
+          // visualizer({
+          // 	emitFile: false,
+          // 	filename: "stats.html",
+          // 	open: true,
+          // 	template: "sunburst",
+          // }),
+      ],
 	},
-	experimental: {
-		contentIntellisense: true,
+
+  experimental: {
+      contentIntellisense: true,
 	},
-	i18n: {
-		locales: ["en", "es"],
-		defaultLocale: "en",
+
+  i18n: {
+      locales: ["en", "es"],
+      defaultLocale: "en",
 	},
+
+  integrations: [icon()],
 });
